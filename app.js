ae5aeaa2f1b29d07c066b3eb3c7b39e37c1eed9e
Geo Boland <Geonalob.school@gmail.com>
2022-11-07 12:40:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+    var button=document.getElementById("btn");
+    var result=document.getElementById("result");
+    var searchValue=document.getElementById("search");
+
+    button.addEventListener("click",function(){
+        Req=new XMLHttpRequest();
+        var url="http://localhost/info2180-lab4/superheroes.php?query="+searchValue.value+"";
+        Req.open('Get',url);
+        Req.send();
+        Req.onreadystatechange=respond;
+
+    });
+    function respond(){
+        if(Req.readyState===XMLHttpRequest.DONE){
+            if(Req.status===200){
+                result.innerHTML=Req.responseText;
+            }else{
+                alert('Error');
+            }
+        }
+    }
+}
