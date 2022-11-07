f1db5fe6a7a5109639bab2d9f4e043e5260e7bcb
Geo Boland <Geonalob.school@gmail.com>
2022-11-07 18:11:07 -0500

    Exercise three complete

--- superheroes.php
+++ superheroes.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 $superheroes = [
   [
       "id" => 1,
@@ -66,7 +66,29 @@ $superheroes = [
 ?>
 
 <ul>
-<?php foreach ($superheroes as $superhero): ?>
-  <li><?= $superhero['alias']; ?></li>
-<?php endforeach; ?>
-</ul>
+        <?php 
+            if($_REQUEST['query'] == ""):
+            // If query value is not set 
+                foreach ($superheroes as $superhero): ?>
+                    <li><?= $superhero['alias']; ?></li>
+                <?php endforeach;
+            else:
+                $search_value = filter_input(INPUT_GET,'query',FILTER_SANITIZE_STRING);
+                //echo "<br>Search value is ".$search_value;
+                $found = false;
+                foreach ($superheroes as $superhero):
+                    if($search_value==$superhero["name"] || $search_value==$superhero["alias"]): ?>
+                        <h3> <?= $superhero['alias']; ?></h3>
+                        <h4> A.K.A. <?= $superhero['name']; ?></h4>
+                        <p><?= $superhero['biography']; ?></p>
+                        <?php $found = true;
+                    endif;
+                endforeach;
+                if ($found==false):
+                {
+                    echo "<h5>SUPERHERO NOT FOUND";
+                }
+                endif;
+            endif; 
+        ?>
+    </ul>
